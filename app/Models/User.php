71cfa328671eaa50e2973b75ba937c332da83d03
anas1412 <anas.bassoumi@gmail.com>
2025-10-14 10:53:19 +0100

    setting up 2 panels and tenants

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use Filament\Models\Contracts\HasTenants;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Collection;
+
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -20,8 +26,26 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        /* 'password', */
+        'google_id', 
+        'tenant_id', 
+        'is_admin',
     ];
+
+    public function tenant(): BelongsTo
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+
+    public function getTenants(Panel $panel): Collection
+    {
+        return collect([$this->tenant]);
+    }
+
+    public function canAccessTenant(Model $tenant): bool
+    {
+        return $this->tenant_id === $tenant->id;
+    }
 
     /**
      * The attributes that should be hidden for serialization.
@@ -29,7 +53,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $hidden = [
-        'password',
+        /* 'password', */
         'remember_token',
     ];
 
@@ -42,7 +66,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            /* 'password' => 'hashed', */
         ];
     }
 }
--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -19,29 +19,35 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Filament\Support\Enums\Width;
+use App\Models\Tenant;
 
-class AdminPanelProvider extends PanelProvider
+class DashboardPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
         return $panel
             ->default()
-            ->id('admin')
-            ->path('admin')
+            ->id('dashboard')
+            ->path('dashboard')
             ->login()
             ->spa()
             ->colors([
-                'primary' => Color::Blue,
+                'primary' => Color::Green,
             ])
             ->maxContentWidth(Width::Full)
             ->unsavedChangesAlerts()
             ->databaseTransactions()
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
+            ->tenant(
+                Tenant::class,
+                slugAttribute: 'slug', // Use the 'slug' column for tenant URLs
+                ownershipRelationship: 'tenant' // The name of the relationship on the User model
+            )
+            ->discoverResources(in: app_path('Filament/Dashboard/Resources'), for: 'App\Filament\Dashboard\Resources')
+            ->discoverPages(in: app_path('Filament/Dashboard/Pages'), for: 'App\Filament\Dashboard\Pages')
             ->pages([
                 Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
+            ->discoverWidgets(in: app_path('Filament/Dashboard/Widgets'), for: 'App\Filament\Dashboard\Widgets')
             ->widgets([
                 AccountWidget::class,
                 FilamentInfoWidget::class,
