b08e4aacc0d3d78068d996a4e1642f8f12e5863a
anas1412 <anas.bassoumi@gmail.com>
2025-11-05 17:54:31 +0100

    added tenants management

---
+++ app/Filament/Resources/Tenants/Pages/EditTenant.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\Tenants\Pages;
+
+use App\Filament\Resources\Tenants\TenantResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditTenant extends EditRecord
+{
+    protected static string $resource = TenantResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ app/Filament/Resources/Tenants/Schemas/TenantForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\Tenants\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class TenantForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('slug')
+                    ->required(),
+                Select::make('type')
+                    ->options(['accounting' => 'Accounting', 'commercial' => 'Commercial'])
+                    ->required(),
+            ]);
+    }
+}
